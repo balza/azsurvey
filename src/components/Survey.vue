e424243bd99eefa24d3c188fc35078d52ac70d4b
paolo <paolo.balzarotti@gmail.com>
2017-02-16 16:41:46 +0100

    First commit

---
+++ src/components/Survey.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <ul id="survey">
+      <li v-for="question in questions">
+        {{ question.message }}
+      </li>
+  </ul>
+
+</template>
+
+
+<script>
+
+export default {
+  name: 'survey',
+  data () {
+    return {
+      questions: [
+        {message: 'question1'},
+        {message: 'question2'}
+      ]
+    }
+  }
+}
+
+</script>
+
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from 'components/Hello'
+import Survey from 'components/Survey'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/survey',
+      name: 'Survey',
+      component: Survey
+    }
+
+  ]
+})
+
