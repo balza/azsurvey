18af6f92e3f96eeb933de2c60c705550e17c934e
paolo <paolo.balzarotti@gmail.com>
2017-03-01 17:05:53 +0100

    Added StarRating component to Survey

--- src/components/Survey.vue
+++ src/components/Survey.vue
@@ -1,29 +1,41 @@
 <template>
   
-  <div id="survey">
-    <ul>
-        <li v-for="question in questions">
-          {{ question.message }}
-        </li>
-    </ul>
+  <div class="tab-pane fade in">
+      <div v-for="question in questions" align="left">
+        {{question.message}}
+          <star-rating 
+            v-bind:star-size="10"
+            v-bind:max-rating="10" 
+            @rating-selected="syncRating">
+        </star-rating/> 
+      </div>
   </div>
 
 </template>
 
 
 <script>
+  import StarRating from 'vue-star-rating'
 
-export default {
-  name: 'survey',
-  data () {
-    return {
-      questions: [
-        {message: 'question1'},
-        {message: 'question2'}
-      ]
+  export default {
+    name: 'survey',
+    data () {
+      return {
+        questions: [
+          {message: 'ti piacciono i fiori?'},
+          {message: 'vuoi bene alla mamma?'}
+        ]
+      }
+    },
+    components: {
+      StarRating
+    },
+    methods: {
+      syncRating (rating) {
+        this.rating = rating
+      }
     }
   }
-}
 
 </script>
 
